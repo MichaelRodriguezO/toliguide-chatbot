527e198e3e4b96762316ddf6e1b7a37bae4d8c03
MichaelRodriguezO <vege10000@gmail.com>
2025-11-09 18:16:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("placeholder main")
+from flask import Flask, render_template, request, jsonify
+from app.controllers.chat_controller import chat_bp
+
+# Crear la app y decirle dónde están las plantillas
+app = Flask(__name__, template_folder="app/templates")
+
+# Registrar el Blueprint
+app.register_blueprint(chat_bp)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"error": "Ruta no encontrada"}), 404
+
+if __name__ == "__main__":
+    # Render requiere host 0.0.0.0
+    app.run(host="0.0.0.0", port=5000)
+
