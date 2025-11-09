3f0674341d27c299247430d1e8519cdfcd85fec9
MichaelRodriguezO <vege10000@gmail.com>
2025-11-09 18:25:16 -0500

    Update info_data.py

--- app/data/info_data.py
+++ app/data/info_data.py
@@ -1,1 +1,12 @@
-INFO={}
+INFO = {
+    "turismo": {
+        "plaza de bolivar": "Centro histórico.",
+        "cañón del combeima": "Naturaleza y senderismo."
+    },
+    "hoteles": {
+        "sonesta hotel ibague": "Hotel 5 estrellas."
+    },
+    "restaurantes": {
+        "la ricotta": "Comida italiana."
+    }
+}
