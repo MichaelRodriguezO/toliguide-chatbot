e5f6f29c96adc9d3b29770f2816c4a9f2963eaaa
MichaelRodriguezO <vege10000@gmail.com>
2025-11-09 18:25:59 -0500

    Update normalizer.py

--- app/utils/normalizer.py
+++ app/utils/normalizer.py
@@ -1,1 +1,8 @@
-print("norm")
+import unicodedata
+
+def normalize(text):
+    text = text.lower()
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', text)
+        if unicodedata.category(c) != 'Mn'
+    )
